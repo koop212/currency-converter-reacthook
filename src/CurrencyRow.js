7e32cf75341b1064d4dc002982a3cf9253b4e925
Bobby Khounphinith <khoupe07@gmail.com>
2020-02-20 21:24:57 -0600

    Ready to deploy

---
+++ src/CurrencyRow.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function CurrencyRow(props) {
+    const {
+        currencyOptions,
+        selectedCurrency,
+        onChangeCurrency,
+        onChangeAmount,
+        amount
+
+    } = props
+    return (
+        <div>
+          <input type="number"  className="input" value={amount} onChange={onChangeAmount} />
+          <select value={selectedCurrency} onChange={onChangeCurrency}>
+              {currencyOptions.map(options => (
+                  <option key={options} value={options}>{options}</option>
+                ))}
+          </select>
+        </div>
+    )
+}
